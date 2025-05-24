8bd736a2ba1dfc34f6e58b9a0e148c69cde7e87e
pradipbkamble <pradipbkamble001@gmail.com>
2025-05-24 23:26:32 +0530

    user login and signup

--- src/components/user-login/user-login.component.ts
+++ src/components/user-login/user-login.component.ts
@@ -1,37 +1,67 @@
 import { CommonModule, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import{MatCard} from '@angular/material/card'
-import{MatInput} from '@angular/material/input'
-import{MatFormFieldModule} from '@angular/material/form-field'
-import{MatButton} from '@angular/material/button'
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatSelect } from '@angular/material/select';
+
 import { UserApiService } from '../../service/user-api.service';
-import { log } from 'node:console';
-
-
-
-
-
 
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule,MatCard,MatButton,MatFormFieldModule,MatInput,NgIf],
+  imports: [
+    CommonModule,
+    MatOptionModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelect,
+    MatCardModule,
+    MatButton,
+    MatFormFieldModule,
+    MatInput,
+    NgIf,
+    MatDatepickerModule,
+  ],
   templateUrl: './user-login.component.html',
-  styleUrl: './user-login.component.scss'
+  styleUrl: './user-login.component.scss',
 })
 export class UserLoginComponent implements OnInit {
-  signInForm!:FormGroup
-  isRegistered=true;
-  constructor(private fb: FormBuilder, private _userApi:UserApiService) {}
- 
+  signInForm!: FormGroup;
+  signUpForm!: FormGroup;
+  isLogin = false;
+  constructor(private fb: FormBuilder, private _userApi: UserApiService) {}
+
   ngOnInit() {
     this.userForm();
+    this. userSignUpForm()
   }
-   userForm() {
+  userForm() {
     this.signInForm = this.fb.group({
       username: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  userSignUpForm() {
+    this.signUpForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      gmail: ['', [Validators.required]],
+      contact: ['', [Validators.required]],
+      securityq:[null],
+      ConfirmPassword: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
@@ -41,16 +71,14 @@ export class UserLoginComponent implements OnInit {
     } else {
       console.log('Form is invalid');
     }
-
   }
-  signIn(){
-    this._userApi.checkUser({...this.signInForm.value}).subscribe(res=>{
+  signIn() {
+   
+    this.isLogin = false;
+  }
+  signUp(){
+     this._userApi.checkUser({ ...this.signUpForm.value }).subscribe((res) => {
       console.log(res);
-      
-    })
-    
-    
+    });
   }
-
-
 }
